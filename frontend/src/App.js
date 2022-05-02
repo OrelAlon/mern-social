17bea740589b7749a53e7733be01b9a2b56a90cc
OrelAlon <orela231089@gmail.com>
2022-05-02 22:24:46 +0300

    add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import Home from "./pages/home/Home";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>Orel</header>
+      <Home />
     </div>
   );
 }
