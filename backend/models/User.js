24c9c37973941236fcaf77edc3eeedc3301d3e86
OrelAlon <orela231089@gmail.com>
2022-05-02 18:22:40 +0300

    added user route

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,14 +4,15 @@ const UserSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      required: true,
-      min: 6,
+      require: true,
+      min: 3,
       max: 20,
       unique: true,
     },
     email: {
       type: String,
       required: true,
+      max: 50,
       unique: true,
     },
     password: {
@@ -38,6 +39,22 @@ const UserSchema = new mongoose.Schema(
     isAdmin: {
       type: Boolean,
       default: false,
+    },
+    desc: {
+      type: String,
+      max: 50,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+    from: {
+      type: String,
+      max: 50,
+    },
+    relationship: {
+      type: Number,
+      enum: [1, 2, 3],
     },
   },
   { timestamps: true }
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,7 +10,7 @@ router.post("/register", async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     // create new user
-    const newUser = await new User({
+    const newUser = new User({
       username: req.body.username,
       email: req.body.email,
       password: hashedPassword,
@@ -19,8 +19,8 @@ router.post("/register", async (req, res) => {
     // save user
     const user = await newUser.save();
     res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
@@ -38,8 +38,8 @@ router.post("/login", async (req, res) => {
     !validPassword && res.status(400).json("wrong password");
 
     res.status(200).json(user);
-  } catch (error) {
-    res.status(400).send(error);
+  } catch (err) {
+    res.status(500).json(err);
   }
 });
 
