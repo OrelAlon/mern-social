c6ade5d2139f4bf3c16d36e77b707792565ecef1
OrelAlon <orela231089@gmail.com>
2022-05-03 11:45:21 +0300

    add navbar

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,9 @@
 import Navbar from "../../components/navbar/Navbar";
+import { Person } from "@material-ui/icons";
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
       <Navbar />
     </div>
   );
