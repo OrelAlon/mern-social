60ae25a6ea71f87db47e712e0a6949645832673e
OrelAlon <orela231089@gmail.com>
2022-06-19 20:38:15 +0300

    work add rest page

--- frontend/src/pages/addRestaurant/AddRestaurant.jsx
+++ frontend/src/pages/addRestaurant/AddRestaurant.jsx
@@ -1,4 +1,7 @@
 import { useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import axios from "axios";
 
 import Navbar from "../../components/navbar/Navbar";
 
@@ -9,13 +12,51 @@ import "./addRestaurant.css";
 const AddRestaurant = () => {
   const [file, setFile] = useState(null);
 
+  const restaurantnameRef = useRef();
+  const descRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const restaurant = {
+      restaurantname: restaurantnameRef.current.value,
+      desc: descRef.current.value,
+    };
+
+    if (file) {
+      const data = new FormData();
+      const fileName = Date.now() + file.name;
+      data.append("name", fileName);
+      data.append("file", file);
+      restaurant.profilePicture = fileName;
+      try {
+        await axios.post("/upload", data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    try {
+      await axios.post("/restaurants/", restaurant);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const submitBtn = () => {
+    navigate("/login");
+  };
+
   return (
     <>
       <Navbar />
       <div className='addRestaurant'>
         <h1>AddRestaurant</h1>
 
-        <form action='submit' className='formBox'>
+        <form action='submit' className='formBox' onSubmit={handleSubmit}>
           <label htmlFor='file' className='loginImg'>
             Add Restaurant Name
           </label>
@@ -24,8 +65,18 @@ const AddRestaurant = () => {
             type='string'
             required
             className='restaurantInput'
-            // ref={emailRef}
+            ref={restaurantnameRef}
           />
+          <label htmlFor='file' className='loginImg'>
+            Add Restaurant Description
+          </label>
+          <input
+            placeholder='Restaurant Description'
+            type='string'
+            className='restaurantInput'
+            ref={descRef}
+          />
+
           <div className='imgLog'>
             <label htmlFor='file' className='loginImg'>
               <PermMedia htmlColor='tomato' className='shareIcon' />
@@ -41,10 +92,9 @@ const AddRestaurant = () => {
               />
             </label>
           </div>
-          {/* <button className='loginButton' type='submit' disabled={isFetching}>
-            {isFetching ? <CircularProgress size='25px' /> : "Login"}
-          </button> */}
-          {/* <span className='loginForgot'>Forgot Password?</span> */}
+          <button className='loginButton' type='submit' onClick={submitBtn}>
+            Add
+          </button>
         </form>
       </div>
     </>
